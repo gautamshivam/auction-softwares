2c0489c52769423616433a1618fe448375dc15d8
shivam gautam <gautamshivam25@gmail.com>
2021-10-30 14:22:38 -0500

    Add files via upload

---
+++ auction-problem3.js
@@ -0,0 +1,25 @@
+
+inputStr = "2 4 7 8 10";
+inputArr = inputStr.split(' ');
+input = []
+inputArr.forEach(item => {
+    input.push(parseInt(item));
+});
+index = 0;
+oddCount = 0;
+evenCount = 0;
+lastEvenIndex = -1;
+lastOddIndex = -1;
+while(index < input.length) {
+    if (input[index] % 2 == 0) {
+        evenCount++;
+        lastEvenIndex = index;
+    } else {
+        oddCount++;
+        lastOddIndex = index;
+    }
+    index = index + 1;
+}
+if(oddCount == 1) {
+    console.log(lastOddIndex+1)
+} else console.log(lastEvenIndex + 1)
